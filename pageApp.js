c7d45481d5b7c13706aeb639a40eda2fe5b551c2
Henrik Bergqvist <henrik.bergqvist@aroskapital.se>
2022-12-06 08:39:01 +0100

    Change in language display

--- pageApp.js
+++ pageApp.js
@@ -4,7 +4,9 @@ var langApp = new Vue({
         res: i18n,
         lang: "en-US",
         medata: me,
-        langdata: [],
+        langdata7: {loaded: false},
+        langdata30: {loaded: false},
+        langdataYear: {loaded: false},
         githubdata: [],
     },
     methods: {
@@ -12,9 +14,20 @@ var langApp = new Vue({
             return this.res.getText(text, this.lang);
         },
         loadData: function() {
-            //this.$http.jsonp('https://wakatime.com/share/@74d4c724-26da-438d-baa7-06026a9391c9/d2be2c53-20ae-4ac4-b92d-42f516260c32.json').then(function(response){
+            this.$http.jsonp('https://wakatime.com/share/@74d4c724-26da-438d-baa7-06026a9391c9/d2be2c53-20ae-4ac4-b92d-42f516260c32.json').then(function(response){
+                this.langdata7 = {
+                    loaded: true,
+                    data: response.body.data,
+                };
+            });
+            this.$http.jsonp('https://wakatime.com/share/@74d4c724-26da-438d-baa7-06026a9391c9/d39c662f-e8d9-4726-bbb4-55538a3f0b69.json').then(function(response){
+                this.langdata30 = {
+                    loaded: true,
+                    data: response.body.data,
+                };
+            });
             this.$http.jsonp('https://wakatime.com/share/@74d4c724-26da-438d-baa7-06026a9391c9/616c84af-bd80-4666-8b90-2ee0ebfd6f29.json').then(function(response){
-                this.langdata = {
+                this.langdataYear = {
                     loaded: true,
                     data: response.body.data,
                 };
@@ -28,8 +41,28 @@ var langApp = new Vue({
         },
     },
     computed: {
+        languagesLoaded: function() {
+            return this.langdata7.loaded && this.langdata30.loaded && this.langdataYear.loaded
+        },
+        languagesMerged: function() {
+            l30 = this.langdata30;
+            l7 = this.langdata7;
+            return _(this.langdataYear.data).map(function(o) {
+                py = o.percent;
+                d30 = _(l30.data).find({ name: o.name });
+                d7 = _(l7.data).find({ name: o.name });
+                p30 = d30 ? d30.percent : 0;
+                p7 = d7 ? d7.percent : 0;
+                return {
+                    name: o.name,
+                    percent: p7,
+                    percent30: p30,
+                    percentYear: py
+                };
+            }).value();
+        },
         orderedLanguages: function() {
-            return _(this.langdata.data).orderBy('percent', 'desc').value();
+            return _(this.languagesMerged).orderBy('percent', 'desc').value();
         },
         githubCommits: function() {
             return _(this.githubdata.data).filter(function(e) {return e.type == 'PushEvent';}).orderBy('created_at', 'desc').value();
