e1e1fa0f64251891b7b2ab9cf9b9a871583ddab5
Henrik Bergqvist <henrik.bergqvist@aroskapital.se>
2024-05-16 12:52:42 +0200

    fix

--- pageApp.js
+++ pageApp.js
@@ -108,7 +108,7 @@ var langApp = new Vue({
         techniques: function() {return _(this.medata.techniques).orderBy('name', 'asc').value();},
         aboutMeAsArray: function() {return this.objectToArray(this.medata.about);},
         linksAsArray: function() {return this.objectToArray(this.medata.links);},
-        years: () => [1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023],
+        years: () => [1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024],
     },
     created: function() {
         this.loadData();
